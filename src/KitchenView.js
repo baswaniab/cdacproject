9438b4e09f046b3ad9c46be60caa32580b2024f3
ashikmohammad <ashikahammedkpm@gmail.com>
2021-08-21 22:51:46 +0530

    modified kitchen view page condition

--- src/KitchenView.js
+++ src/KitchenView.js
@@ -4,8 +4,9 @@ import {useGlobalContext} from './context'
 
 export default function Kitchen(){
     const {id}=useParams();
+    console.log(typeof id);
     const {kitchen}=useGlobalContext();
-    const kitchenDetail=kitchen.filter((item)=> item.id==id )
+    const kitchenDetail=kitchen.filter((item)=> item.id===id )
     // console.log(kitchen);
     return <>
         <img src={kitchenDetail[0].image} alt="" style={{width:"100%",height:"290px"}} />
