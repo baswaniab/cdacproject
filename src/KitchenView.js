fcd6dd23bda88b933242cc3e22e64c8d1d8c463d
ashikmohammad <ashikahammedkpm@gmail.com>
2021-08-22 13:39:51 +0530

    added kitchen component

--- src/KitchenView.js
+++ src/KitchenView.js
@@ -7,9 +7,18 @@ export default function Kitchen(){
     const {kitchen}=useGlobalContext()
     const kitchenDetails=kitchen.find((item)=> item.id===id )
     if(kitchenDetails){
-        return <img src={kitchenDetails.image} alt="" style={{width:"100%",height:"290px"}}></img>
+        
+        return <>
+        <KitchenComponent kitchen={kitchenDetails}/>
+        </>
     }
     else{
         return <h2>loading</h2>
     }
+}
+
+const KitchenComponent=(props)=>{
+    const kitchenDetails=props.kitchen;
+    console.log(kitchenDetails);
+    return <img src={kitchenDetails.image} alt="" style={{width:"100%",height:"290px"}}></img>
 }
